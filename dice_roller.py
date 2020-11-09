4a3b2d2881be3184c0e77a188afd2ac3357c3ee8
jicsontoh <64951628+jicsontoh@users.noreply.github.com>
2020-11-09 16:52:10 +0800

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -5,9 +5,8 @@ def main():
   dice_sum = 0
   for i in range(0, dice_roll):
     roll = random.randint(1,6)
-    print(f'You rolled a {roll}')
-    
-  dice_sum += roll
+    print(f'You rolled a {roll}')    
+    dice_sum += roll
   print(f'You rolled a total of {dice_sum}')
 if __name__ == "__main__":
   main()
